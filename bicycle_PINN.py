2b45de63b67cd594168ee3cbd9bf8545fbdda454
nvaltekar <nvaltekar@email.arizona.edu>
2022-11-11 15:34:36 -0700

    first attempt

--- bicycle_PINN.py
+++ bicycle_PINN.py
@@ -20,6 +20,7 @@ class bicycle_PINN:
         self.control = df[["speed",'steering_angle',"length","timestamp_posix"]]
         self.XTFC(self.control.shape,self.states.shape,self.n_nodes)
 
+        
         control_train=self.control.loc[:int(len(self.control)*self.ratio)]
         control_test=self.control.loc[int(len(self.control)*self.ratio):]
         states_train=self.states.loc[:int(len(self.states)*self.ratio)]
@@ -38,7 +39,7 @@ class bicycle_PINN:
         self.b=tf.random.uniform(minval=LBw,maxval=UBw,shape=(n_nodes,1))
         self.betas = tf.Variable(np.ones((n_nodes,states_shape[1])),dtype='float32')
         
-    def train(self,activation_function,control,states,opt,n_epochs=10000):
+    def train(self,activation_function,control,states,opt):
         
         states_shape = states.shape
         u0 = np.array(states.loc[0])
@@ -48,24 +49,23 @@ class bicycle_PINN:
         H = (X-lb)/(ub-lb)
         H =  tf.convert_to_tensor(H[:,3],dtype='float32')
         states_norm = (states-states.min())/(states.max()-states.min())
-        for i in range(n_epochs):
-        
-            with tf.GradientTape(persistent=True) as g:
-                g.watch(H)
-                g.watch(self.betas)
-                pred = tf.matmul(tf.tanh(tf.transpose(self.b) +tf.matmul(tf.transpose([H]),self.W)),(self.betas))
-                sech_=1-tf.pow(tf.tanh(tf.transpose(self.b) +tf.matmul(tf.transpose([H]),self.W)),2)
-                right_mult = tf.math.multiply(sech_,self.W)
-                dH_dt = tf.matmul(right_mult,self.betas)
-                x_t = dH_dt[:,0]
-                y_t = dH_dt[:,1]
-                theta_t = dH_dt[:,2]
-                f1 = x_t - (X[:,0]*tf.cos(pred[:,2]))
-                f2 = y_t - (X[:,0]*tf.sin(pred[:,2]))
-                f3 = theta_t - (X[:,0]*X[:,1]/X[:,2])
-                b = pred[0,:]-u0
-                loss = tf.reduce_mean(tf.square(pred-states_norm)+tf.square(tf.norm([f1+f2+f3]))+tf.square(tf.norm(b)))
-            grads = g.gradient(loss,self.betas)
-            opt.apply_gradients(zip([grads],[self.betas]))
-            print("Training loss at step %d: %.4f"%(i, float(loss)))
-                
+    
+        with tf.GradientTape(persistent=True) as g:
+            g.watch(H)
+            g.watch(self.betas)
+            pred = tf.matmul(tf.tanh(tf.transpose(self.b) +tf.matmul(tf.transpose([H]),self.W)),(self.betas))
+            sech_=1-tf.pow(tf.tanh(tf.transpose(self.b) +tf.matmul(tf.transpose([H]),self.W)),2)
+            right_mult = tf.math.multiply(sech_,self.W)
+            dH_dt = tf.matmul(right_mult,self.betas)
+            x_t = dH_dt[:,0]
+            y_t = dH_dt[:,1]
+            theta_t = dH_dt[:,2]
+            f1 = x_t - (X[:,0]*tf.cos(pred[:,2]))
+            f2 = y_t - (X[:,0]*tf.sin(pred[:,2]))
+            f3 = theta_t - (X[:,0]*X[:,1]/X[:,2])
+            b = pred[0,:]-u0
+            loss = tf.reduce_mean(tf.square(pred-states_norm)+tf.square(tf.norm([f1+f2+f3]))+tf.square(tf.norm(b)))
+        grads = g.gradient(loss,self.betas)
+        opt.apply_gradients(zip([grads],[self.betas]))
+        print("Training loss at step %d: %.4f"%(i, float(loss)))
+            
