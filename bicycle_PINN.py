a1ea316178073cf01a7f5ce8fe2ceae27ae22fc4
Cornejo <samuelcornejo@arizona.edu>
2022-11-17 16:32:23 -0700

    added XTFC

--- bicycle_PINN.py
+++ bicycle_PINN.py
@@ -19,13 +19,14 @@ class bicycle_PINN:
         self.states = df[['x','y','heading']]
         self.control = df[["speed",'steering_angle',"length","timestamp_posix"]]
         self.XTFC(self.control.shape,self.states.shape,self.n_nodes)
-
         
-        control_train=self.control.loc[:int(len(self.control)*self.ratio)]
-        control_test=self.control.loc[int(len(self.control)*self.ratio):]
+        #control_train=self.control.loc[:int(len(self.control)*self.ratio)]
+        #control_test=self.control.loc[int(len(self.control)*self.ratio):]
         states_train=self.states.loc[:int(len(self.states)*self.ratio)]
         states_test=self.states.loc[int(len(self.states)*self.ratio):]
-        self.train(self.act,control_train,states_train)
+        self.train(self.act,self.control,states_train,states_test,"PIELM")
+        self.XTFC(self.control.shape,self.states.shape,self.n_nodes)
+        self.train(self.act,self.control,states_train,states_test,"XTFC")
         
 
     def XTFC(self,control_shape,states_shape,n_nodes):
@@ -38,40 +39,79 @@ class bicycle_PINN:
         self.b=tf.random.uniform(minval=LBb,maxval=UBb,shape=(n_nodes,1))
         self.betas = tf.Variable(np.ones((n_nodes,states_shape[1])),dtype='float32')
         
-    def train(self,activation_function,control,states):
+    def train(self,activation_function,control,states,states_test,mode):
         
         states_shape = states.shape
         u0 = np.array(states.loc[0])
-        X = np.array(control)
-        now = datetime.datetime.now()
-    
-        #bound input time from 0 to 1 
-        x_max = 1
-        x_min = 0
-        c = (x_max-x_min) /(X[:,3].max()-X[:,3].min())
-        H =  tf.convert_to_tensor(c*X[:,3],dtype='float32')
-        Y = np.array(states)
-        H_ = tf.tanh(tf.transpose(self.b) +tf.matmul(tf.transpose([H]),self.W))
-        pred = tf.matmul(H_,(self.betas))
-        print(tf.reduce_sum(tf.math.pow(pred-Y,2)))
-        sech_=1-tf.pow(tf.tanh(tf.transpose(self.b) +tf.matmul(tf.transpose([H]),self.W)),2)
-        right_mult = tf.math.multiply(sech_,self.W)
-        dH_dt_ = right_mult*c
-        A = tf.concat([H_,dH_dt_],axis=0)
-        xt=(tf.transpose([X[:,0]*tf.cos(pred[:,2])]))
-        yt=(tf.transpose([X[:,0]*tf.sin(pred[:,2])]))
-        thetat=(tf.transpose([X[:,0]*X[:,1]/X[:,2]]))
-        thetat =tf.cast(thetat,tf.float32)
-        U =tf.concat([xt,yt],axis=1)
-        U = tf.concat([U,thetat],axis=1)
-        B = tf.concat([Y,U],axis=0)
-        self.betas = tf.linalg.lstsq(A,B,fast=False)
-        after = datetime.datetime.now()
-        print(after-now)
-        new_preds = tf.matmul(H_,(self.betas))
-        print(tf.reduce_sum(tf.math.pow(new_preds-Y,2)))
+        
+        
+        if mode=="PIELM":
 
-         # with tf.GradientTape(persistent=True) as g:
+            X = np.array(control)
+            now = datetime.datetime.now()
+    
+            #bound input time from 0 to 1 
+            x_max = 1
+            x_min = 0
+            c = (x_max-x_min) /(X[:,3].max()-X[:,3].min())
+            H =  tf.convert_to_tensor(c*X[:,3],dtype='float32')
+            Y = np.array(states)
+            H_ = tf.tanh(tf.transpose(self.b) +tf.matmul(tf.transpose([H]),self.W))
+            pred = tf.matmul(H_,(self.betas))
+            sech_=1-tf.pow(tf.tanh(tf.transpose(self.b) +tf.matmul(tf.transpose([H]),self.W)),2)
+            right_mult = tf.math.multiply(sech_,self.W)
+            dH_dt_ = right_mult*c
+            A = tf.concat([H_,dH_dt_],axis=0)
+            xt=(tf.transpose([X[:,0]*tf.cos(pred[:,2])]))
+            yt=(tf.transpose([X[:,0]*tf.sin(pred[:,2])]))
+            thetat=(tf.transpose([X[:,0]*X[:,1]/X[:,2]]))
+            thetat =tf.cast(thetat,tf.float32)
+            U =tf.concat([xt,yt],axis=1)
+            U = tf.concat([U,thetat],axis=1)
+            B = tf.concat([Y,U],axis=0)
+            self.betas = tf.linalg.lstsq(A,B,fast=False)
+            after = datetime.datetime.now()
+            print(after-now)
+            new_preds = tf.matmul(H_,(self.betas))
+            print(tf.reduce_sum(tf.math.pow(new_preds-Y,2)))
+            return new_preds
+            
+        elif mode=="XTFC":
+
+            X = np.array(control)
+            now = datetime.datetime.now()
+    
+            #bound input time from 0 to 1 
+            x_max = 1
+            x_min = 0
+            c = (x_max-x_min)/(X[:,3].max()-X[:,3].min())
+            H =  tf.convert_to_tensor(c*X[:,3],dtype='float32')
+            Y = np.array(states)
+            H_ = tf.tanh(tf.transpose(self.b) +tf.matmul(tf.transpose([H]),self.W))
+            F_=(H_-H_[0,:])
+            C_ = Y-Y[0,:]
+            pred = tf.matmul(H_,(self.betas))
+            sech_=1-tf.pow(tf.tanh(tf.transpose(self.b) +tf.matmul(tf.transpose([H]),self.W)),2)
+            right_mult = tf.math.multiply(sech_,self.W)
+            dH_dt_ = right_mult*c
+            A = tf.concat([F_,dH_dt_],axis=0)
+            xt=(tf.transpose([X[:,0]*tf.cos(pred[:,2])]))
+            yt=(tf.transpose([X[:,0]*tf.sin(pred[:,2])]))
+            thetat=(tf.transpose([X[:,0]*X[:,1]/X[:,2]]))
+            thetat =tf.cast(thetat,tf.float32)
+            U =tf.concat([xt,yt],axis=1)
+            U = tf.concat([U,thetat],axis=1)
+            B = tf.concat([C_,U],axis=0)
+            self.betas = tf.linalg.lstsq(A,B,fast=False)
+            after = datetime.datetime.now()
+            print(after-now)
+            new_preds = tf.matmul(F_,(self.betas))+Y[0,:]
+            print(tf.reduce_sum(tf.math.pow(new_preds-Y,2)))
+            return new_preds
+
+
+
+        # with tf.GradientTape(persistent=True) as g:
         #     g.watch(H)
         #     g.watch(self.betas)
         #     dH_dt = tf.matmul(right_mult,self.betas)
